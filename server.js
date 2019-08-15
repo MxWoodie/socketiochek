536e33d1b177cedb9ce1a50c3a137ea39f19378a
MxWoodie <maxswation@gmail.com>
2019-08-15 22:25:28 +0300

    Complete getting started

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message',  msg);
+  });
+  console.log('a user connected!');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
